e290741b63aa6611ce15d83d0e18d98d8169875e
lakhansamani <lakhan.m.samani@gmail.com>
2015-12-05 10:18:24 +0530

    Adds basic board

---
+++ models/category.js
@@ -0,0 +1,20 @@
+var mongoose=require('mongoose');
+var schema=mongoose.Schema({
+	name:{
+		type:String,
+		required:true,
+		unique:true
+	},
+	description:{
+		type:String,
+		required:true
+	},
+	modified:{
+		type:Date,
+		default:Date.now
+	},
+	user_id:{
+		type:String
+	}
+});
+module.exports=mongoose.model("Category",schema);
